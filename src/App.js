011fcf554244525ebb769ea9a7ab77b48429763c
Lawal Rashidi <lawalrashidi95@gmail.com>
2024-06-14 11:12:32 +0100

    updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
-import "./App.css";
+import React from "react";
+// import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Users from "./users/pages/Users";
+import { Redirect, Switch } from "react-router-dom/cjs/react-router-dom.min";
+import NewPlace from "./places/pages/NewPlace";
 
 function App() {
-  return <h1 className="App">Let's Start!!!</h1>;
+  return (
+    <Router>
+      <Switch>
+      <Route path="/" exact>
+        <Users />
+      </Route>
+      <Route path="/places/new">
+        <NewPlace />
+      </Route>
+      <Redirect to="/" />
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
