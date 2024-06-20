642f5369f4773e33442d5759102915cda572b439
Lawal Rashidi <lawalrashidi95@gmail.com>
2024-06-20 10:51:50 +0100

    updated

--- src/users/pages/Users.js
+++ src/users/pages/Users.js
@@ -3,13 +3,13 @@ import UsersList from "../components/UsersList";
 
 const Users = () => {
   const USERS = [
-    // {
-    //   id: "u1",
-    //   name: "azeez ola",
-    //   image:
-    //     "https://images.unsplash.com/photo-1718056514261-bb037cfe6ddd?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-    //   places: 3,
-    // },
+    {
+      id: "u1",
+      name: "azeez ola",
+      image:
+        "https://images.unsplash.com/photo-1718056514261-bb037cfe6ddd?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      places: 3,
+    },
   ];
   return <UsersList items={USERS} />;
 };
