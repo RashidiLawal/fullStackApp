b373de793a7345df78fdb6490d0d3685da2b1e73
Lawal Rashidi <lawalrashidi95@gmail.com>
2024-08-23 13:09:22 +0100

    updated

--- src/users/pages/Auth.js
+++ src/users/pages/Auth.js
@@ -1,49 +1,58 @@
-import React from 'react'
-import useForm from '../../shared/hooks/form-hook'
-import { VALIDATOR_EMAIL, VALIDATOR_MIN } from '../../shared/util/validators'
-// import Button from '../../shared/components/FormElements/Button'
-import Input from '../../shared/components/FormElements/Input'
-
+import React from "react";
+import useForm from "../../shared/hooks/form-hook";
+import { VALIDATOR_EMAIL, VALIDATOR_MINLENGTH } from "../../shared/util/validators";
+import Button from '../../shared/components/FormElements/Button'
+import Input from "../../shared/components/FormElements/Input";
+import Card from "../../shared/components/UIElements/Card";
+import "./Auth.css";
 
 const Auth = () => {
+  const [formState, inputHandler] = useForm({
+    email: {
+      value: "",
+      isValid: false,
+    },
+    password: {
+      value: "",
+      isValid: false,
+    },
+  });
 
-    const [formState, inputHandler] = useForm({
-        email: {
-            value:"",
-            isValid: false
-        },
-        password: {
-            value:"",
-            isValid: false
-        }
-    })
-    
-    const authenticationHandler = event => {
-        event.preventDefault();
-        console.log(formState.inputs);// send this to the Backend
-    }
+  const authSubmitHandler = (event) => {
+    event.preventDefault();
+    console.log(formState.inputs); // send this to the Backend
+  };
   return (
-    <form className="place-form" onSubmit={authenticationHandler}>
+    <Card className="authentication">
+        <h2>
+            Login Required
+        </h2>
+        <hr />
+      <form className="place-form" onSubmit={authSubmitHandler}>
         <Input
-            id="email"
-            element="input"
-            type="text"
-            label="Email"
-            errorText="The Email Provided is Incorrect"
-            validators={[VALIDATOR_EMAIL()]}
-            onInput={inputHandler}
+          id="email"
+          element="input"
+          type="email"
+          label="E-mail"
+          errorText="The Email Provided is Incorrect"
+          validators={[VALIDATOR_EMAIL()]}
+          onInput={inputHandler}
         />
         <Input
-            id="password"
-            element="input"
-            type="text"
-            label="Password"
-            errorText="You Have Enter a Wrong Password"
-            validators={[VALIDATOR_MIN(7)]}
-            onInput={inputHandler}
+          id="password"
+          element="input"
+          type="password"
+          label="Password"
+          errorText="Enter a Valid Password, at least 5 characters"
+          validators={[VALIDATOR_MINLENGTH(5)]}
+          onInput={inputHandler}
         />
-    </form>
-  )
-}
+        <Button type="submit" disabled={!formState.isValid}>
+            LOGIN
+        </Button>
+      </form>
+    </Card>
+  );
+};
 
-export default Auth
+export default Auth;
